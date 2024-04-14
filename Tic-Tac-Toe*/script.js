ee9bffb3c1a10800bdad26ba1065722e0009b5a8
murilosoarez <murilosoliveira18@gmail.com>
2024-04-14 14:50:00 -0400

    [Add tie event]

--- Tic-Tac-Toe*/script.js
+++ Tic-Tac-Toe*/script.js
@@ -1,116 +1,26 @@
+// Resultado para caso tenha empate
 
 function Gameboard() {
 
-    const createGameboard = (function () {
-        return [[''], [''], [''],
-        [''], [''], [''],
-        [''], [''], ['']]
-    })();
+    const createGameboard = (board) => {
+        for (i=0; i < 9; i++) {
+            let section = document.createElement('section')
+            section.innerHTML = ''
+            board.append(section)
+        }
+    }
 
     const winningOutcomes = (function () {
         return [[[0], [3], [6]], [[1], [4], [7]], [[2], [5], [8]], [[0], [1], [2]], [[3], [4], [5]], [[6], [7], [8]], [[0], [4], [8]], [[6], [4], [2]]]
     })()
 
-    return { createGameboard, winningOutcomes }
-}
-
-
-function Game() {
-
-    // Inicializa o game
-    let gameboard = Gameboard()
-    let board = gameboard.createGameboard
-    let winning = gameboard.winningOutcomes
-
-    let currentPlayer = prompt('Digite seu player: "X" ou "O": ')
-    let user = User(currentPlayer)
-    
-    let counter = 0
-    let spots = 0
-    let mark = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-
-    const findWinner = () => {
-
-        for (i = 0; i < winning.length; i++) {
-            for (q = 0; q < 3; q++) {
-                let index = winning[i][q][0]
-                if (board[index][0] == currentPlayer[0]) {
-                    counter++
-                }
-            }
-
-            if (counter > 2) {
-                console.log(`${currentPlayer} won! ${counter}`)
-                return true
-            }
-            counter = 0
-        }
-    }
-
-    while (true) {
-
-        
-        while (true) {
-
-            let play = prompt(`Choose a place: \n[[${mark[0]}], [${mark[1]}], [${mark[2]}], \n[${mark[3]}], [${mark[4]}], [${mark[5]}],\n[${mark[6]}], [${mark[7]}], [${mark[8]}]]`)
-
-            if (board[play] == '') {
-
-                board[play] = user.getPlayer(currentPlayer)
-                spots = spots + 1
-                
-                for (i=0; i < mark.length; i++) {
-                    if (mark[i] == play) {
-                        mark[i] = user.getPlayer(currentPlayer)
-                    }
-                }
-                
-                break
-            }
-            
-        }
-        
-        findWinner()
-       
-        while (true) {
-            
-            let random = Math.floor(Math.random() * 9)
-            currentPlayer = user.switchPlayer(currentPlayer)
-
-            if (board[random] == '') {
-
-                board[random] = user.getPlayer(currentPlayer)
-                spots = spots + 1
-
-                for (i=0; i < mark.length; i++) {
-                    if (mark[i] == random) {
-                        mark[i] = user.getPlayer(currentPlayer)
-                    }
-                }
-                
-                break
-            }
-
-            currentPlayer = user.switchPlayer(currentPlayer)
-        }  
-
-        findWinner()
-
-        if (findWinner()) { break }
-
-        if (spots > 8) { 
-            console.log('Its a tie!')
-            break
-        }
-
-        currentPlayer = user.switchPlayer(currentPlayer)
-    }
-
+    return { createGameboard , winningOutcomes }
 }
 
 function User() {
 
     const getPlayer = (player) => { return player }
+
     const switchPlayer = (player) => {
         if (player === 'X') {
             return 'O'
@@ -123,7 +33,122 @@ function User() {
     return { getPlayer, switchPlayer }
 }
 
-Game()
+function Game() {
 
+    const checkSections = (winning, board, player) => {
 
+        let counter = 0
+        let winningCard
 
+        for (i=0; i < winning.length; i ++) {
+            for (q = 0; q < 3; q ++) {
+                let index = winning[i][q][0]
+                if (board[index].innerHTML == player) {
+                    counter ++
+                    if (counter == 3) { return counter }
+                }
+            }
+            counter = 0 
+        }
+
+        return counter
+
+    }
+
+    const checkWin = (counter) => {
+        if (counter == 3) {
+            return true
+        }
+    }
+
+    const opponentPlay = (play, player, spots) => {
+        while (true && spots < 8) {
+            let random = Math.floor(Math.random() * 9)
+            if (play[random].innerHTML === '') { 
+                play[random].innerHTML = player 
+                break
+            }
+        }
+    }
+
+    const finishGame = (showResult, player) => {
+        showResult.innerHTML = `<p>${player} won!</p>`
+    }
+
+    const checkTies = (section) => {
+        let spots = 0
+
+        for (i=0; i < section.length; i++) {
+            if (section[i].innerHTML !== '') {
+                spots ++
+            }
+        }
+
+        return spots
+    }
+
+    return {checkSections, checkWin, opponentPlay, finishGame, checkTies}
+}
+
+function DOMInteraction() {
+    
+    let dialog = document.querySelector('dialog')
+    let players = document.querySelectorAll('.Player')
+    let TicTacToe = document.querySelector('.Tic.Tac.Toe')
+    
+    let user = User()
+    let currentPlayer
+
+    let game = Game()
+    
+    players.forEach((player) => {
+        player.addEventListener('click', () => {
+            currentPlayer = user.getPlayer(player.id)
+            dialog.style.display = 'none'
+        })
+    })
+    
+    let gameboard = Gameboard()
+    gameboard.createGameboard(TicTacToe)
+    let winning = gameboard.winningOutcomes
+    
+    let section = document.querySelectorAll('section')
+    let showResult = document.querySelector('.Result')
+
+    let result = false
+    let tie = 0
+
+    let counter
+    section.forEach((sect) => {
+        sect.addEventListener('click', () => {
+            
+            if (result == false || result == undefined) {
+                sect.innerHTML = currentPlayer
+                tie = game.checkTies(section)            
+                counter = game.checkSections(winning, section, currentPlayer)
+                result = game.checkWin(counter)
+            }
+            
+            if (result) { game.finishGame(showResult, currentPlayer) } 
+            
+            if (result == false || result == undefined) {
+                currentPlayer = user.switchPlayer(currentPlayer) 
+                tie = game.checkTies(section)            
+                game.opponentPlay(section, currentPlayer, tie)
+                counter = game.checkSections(winning, section, currentPlayer)
+                result = game.checkWin(counter)
+            }
+            
+            if (result) { game.finishGame(showResult, currentPlayer) } 
+            currentPlayer = user.switchPlayer(currentPlayer)
+            
+            if (result == undefined && tie > 8) {
+                result = true
+                game.finishGame(showResult, "No one ")
+            }
+
+        })
+    })
+}
+
+document.addEventListener('DOMContentLoaded', DOMInteraction)
