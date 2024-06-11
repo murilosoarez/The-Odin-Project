6ae40d185b5c90a39e7b6d76683f4f7a8dd2deb4
murilosoarez <murilosoliveira18@gmail.com>
2024-06-11 10:18:29 -0400

    [Init general structure]

--- To-Do/src/index.js
+++ To-Do/src/index.js
@@ -1,32 +1,154 @@
 import './styles.css'
 
-function render() {
-    const home = (final) => {
-        const h1 = document.createElement('h1')
-        h1.textContent = 'Home'
-        final.append(h1)
+
+function DOMInteraction() {
+    toggleDialogue()
+    toggleProjects()
+    renderPages()
+}
+
+
+document.addEventListener('DOMContentLoaded', DOMInteraction)
+
+
+function renderPages() {
+
+    function render() {
+
+        const container = document.querySelector('.Right')
+    
+        const home = () => {
+            for (let i=0; i < 12; i++) {
+                const div = document.createElement('div')
+                div.textContent = 'Aquafella'
+                container.append(div)
+            }
+        }
+    
+        const today = () => {
+            const div = document.createElement('div')
+            div.textContent = 'today'
+            container.append(div)
+        }
+    
+        const week = () => {
+            const div = document.createElement('div')
+            div.textContent = 'week'
+            container.append(div)
+        }
+    
+    
+        const favorites = () => {
+            const div = document.createElement('div')
+            div.textContent = 'favorites'
+            container.append(div)
+        }
+    
+        
+    
+        return { home, today, week, favorites }
     }
 
-    return { home }
+    const Box = document.querySelectorAll('.Box')
+    const rendering = render()
+
+    for (let i = 0; i < Box.length; i++) {
+        Box[i].addEventListener('click', () => {
+
+            removeElements()
+
+            if (Box[i].id == 'home') {
+                rendering.home()
+            }
+
+            else if (Box[i].id == 'today') {
+                rendering.today()
+            }
+
+            else if (Box[i].id == 'this-week') {
+                rendering.week()
+            }
+
+            else if (Box[i].id == 'favorites') {
+                rendering.favorites()
+            }
+
+        })
+    }
+
+
 }
-document.addEventListener('DOMContentLoaded', () => {
+
+function toggleProjects() {
+    const button = document.querySelector('#project-button')
+    const list = document.querySelector('#project-list')
+    button.addEventListener('click', () => {
+        list.classList.toggle('Open')
+    })
+}
+
+function toggleDialogue() {
+
     const button = document.querySelector('.Button')
     const dialog = document.querySelector('.Dialog')
     const main = document.querySelector('main')
-    const rendering = render()
-
-    const right = document.querySelector('.Right')
     let count = 0
+
     button.addEventListener('click', () => {
-        dialog.classList.toggle('Open')
+        
         if (count == 0) {
-            main.style.filter = 'blur(2px)'
+            dialog.classList.toggle('Open')
+            main.style.filter = 'blur(15px)'
             count += 1
-            rendering.home(right)
         }
-        else {
-            main.style.filter = 'blur(0px)'
-            count = 0
-        }
-   })
-})
+
+    })
+
+    const close = document.querySelector('#close-button')
+        close.addEventListener('click', () => {
+            if (count == 1) {
+                dialog.classList.toggle('Open')
+                main.style.filter = 'blur(0px)'
+                count -= 1
+            }
+        })
+
+    formInteraction()
+
+}
+
+function formInteraction() {
+    const submit = document.querySelector('#project-submit')
+    const form = document.querySelector('#project-form')
+
+    submit.addEventListener('click', (e) => {
+        e.preventDefault()
+        const formData = new FormData(form)
+        const name = formData.get('name')
+        renderNewProject(name)
+    })
+    
+}
+
+function renderNewProject(name) {
+    const list = document.querySelector('.List')
+    const ul = list.firstElementChild
+    
+    const li = document.createElement('li')
+    ul.append(li)
+    const button = document.createElement('button')
+    button.innerHTML = name
+    li.append(button)
+    
+
+}
+
+
+
+
+function removeElements() {
+    const container = document.querySelector('.Right')
+    while (container.hasChildNodes()) {
+        container.removeChild(container.firstChild)
+    }
+}
